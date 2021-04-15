b9293087539500d7bb2ff0ede8b5579552e8922e
Andres Martinez <andres_m559@Andress-MBP.attlocal.net>
2021-04-15 14:43:37 -0700

    finished structured english

---
+++ test1.py
@@ -0,0 +1,16 @@
+
+
+"""
+The program is trying to determine which one of the two options would return more money after 10 days.
+Option 1 pays 100 dollars a day for 10 days.
+Option 2 pays 1 dollar on the first day, and it continues to double after every single day for ten days.
+This program will use functions to determine which payment option is better.
+function1 will be for Option 1, while function2 will be for Option 2.
+
+function1 will return 100 * 10.
+function1 will work as a loop and continue doubling the amount every day for 10 days.
+
+If both sums are equal, the system will ouput "Option 1 and Option 2 pays the same"
+If Option 1 is greater than Option 2, the system will output "Option 1 is better"
+If Option 2 is greater than Option 1, the system will output "Option 2 is better"
+"""
